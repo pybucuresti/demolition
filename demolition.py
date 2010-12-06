bfb43619b53f442853aaf1c4eafacbece8a85369
catalin badea <catalin.badea@infoexcel.edu.ro>
2010-12-06 20:32:50 +0200

    Red square drops dead.

--- demolition.py
+++ demolition.py
@@ -7,6 +7,9 @@ from pygame.locals import *
 from pygame.color import *
 import pymunk
 import math
+def to_pygame(p):
+    """Small hack to convert pymunk to pygame coordinates"""
+    return int(p.x), int(-p.y+600)
 
 class Block(object):
     def __init__(self, space, pos_x, pos_y, width, height):
@@ -15,17 +18,11 @@ class Block(object):
         corners = [(0,0), (width,0), (width, height), (0, height)]
         contour = pymunk.Poly(body, corners, offset=(-width/2, -height/2))
         space.add(contour, body)
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.corners = corners
-        self.width = width
-        self.height = height
+        self.contour = contour
 
     def draw(self, screen):
-        rect = pygame.Rect(self.pos_x - self.width,
-                           self.pos_y - self.height,
-                           self.width, self.height)
-        pygame.draw.rect(screen, THECOLORS['red'], rect)
+        pointlist = [to_pygame(point) for point in self.contour.get_points()]
+        pygame.draw.polygon(screen, THECOLORS['red'], pointlist)
 
 def main():
     pygame.init()
@@ -38,8 +35,7 @@ def main():
     space = pymunk.Space()
     space.gravity = (0.0, -900.0)
 
-    blocks = [Block(space, 200, 200, 50, 50)]
-    
+    blocks = [Block(space, 200, 600, 50, 50)]
     while running:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -49,7 +45,7 @@ def main():
         
         screen.fill(THECOLORS["white"])
         
-        #space.step(1/50.0)
+        space.step(1/50.0)
 
         for block in blocks:
             block.draw(screen)
